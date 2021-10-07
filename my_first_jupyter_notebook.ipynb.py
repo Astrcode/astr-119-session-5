e4ad286a806a085bc6e2be4abe158b2077ba0f3e
Astrcode <91351020+Astrcode@users.noreply.github.com>
2021-10-07 11:50:32 -0700

    Add files via upload

---
+++ my_first_jupyter_notebook.ipynb.py
@@ -0,0 +1,22 @@
+{
+ "cells":[
+{
+	"cell_type":"code",
+	"execution_count": null,
+	"metadata":{}
+	"outputs":[],
+	"source":[
+	"import numpy as np"
+	]
+},
+{
+"cell_type":"code",
+"execution_count": null,
+"metadata":{}
+"outputs":[],
+"source":[
+"n =10 								#define an integer 10\n",
+"x = np.arange(d,dtype=float)		#define an array x=[0,9]"
+]
+},
+}
